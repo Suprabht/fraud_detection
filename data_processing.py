0f2d4ffcf154efe404a6d1a30f4d1d8c46237522
Suprabhat <suprabhatpaul@gmail.com>
2021-10-26 23:01:14 +0530

    new Incorrect checkin

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,8 @@
 print("Adding all code for data processing...")
 a=9
-b=8
+b=6
 def sum(a,b):
     c=a+b
     return a
-print("add a new function sum which is not correct")
+    
+print("added a new function sum which is not corrwdt")
