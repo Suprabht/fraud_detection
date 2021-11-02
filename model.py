268a645a9289ee367c11a63982c6ede0051046a8
Suprabhat <suprabhatpaul@gmail.com>
2021-11-02 23:49:37 +0530

    added all  ml model code

--- model.py
+++ model.py
@@ -1,1 +1,9 @@
 print("Adding all ML related code")
+def model1():
+    print("Adding all ML model 1")
+
+def model2():
+    print("Adding all ML model 2")
+
+def model3():
+    print("Adding all ML model 3")
